2e38da303280191cf0baa2375ddea2e70ba4af62
Jai Smith <jksmithnyc@gmail.com>
2023-12-29 15:50:13 -0500

    add logs to access function

--- backend/src/handlers/access.py
+++ backend/src/handlers/access.py
@@ -11,22 +11,27 @@ def handler(event, _context):
   historical_fcst_horizon = int(query_params.get('historical_fcst_horizon', 0))
 
   if start_ts >= datetime.now().timestamp():
+    print('start date is in the future, skipping historical query')
     hist = []
   else:
     hist = db.get_hist_entries_after(constants.USGS_SITE, start_ts)
     hist = pd.DataFrame(hist)
     hist = hist.set_index(pd.to_datetime(hist['timestamp'].apply(pd.to_numeric), unit='s'))
+    print(f'retrieved {len(hist.index)} historical observations')
 
   last_fcst_entry = db.get_latest_fcst_entry(constants.USGS_SITE)
   fcst = db.get_entire_fcst(constants.USGS_SITE, last_fcst_entry['origin'])
   fcst = pd.DataFrame(fcst)
   fcst = fcst.set_index(pd.to_datetime(fcst['timestamp'].apply(pd.to_numeric), unit='s'))
+  print(f'retrieved {len(fcst.index)} current forecasted observations')
 
   historical_fcsts = []
   if (historical_fcst_horizon > 0):
+    print('historical forecast horizon provided, fetching historical forecasts')
     historical_fcsts = db.get_fcsts_with_horizon_after(constants.USGS_SITE, historical_fcst_horizon, start_ts)
-    historical_fcsts = pd.DataFrame(fcst)
-    historical_fcsts = fcst.set_index(pd.to_datetime(fcst['timestamp'].apply(pd.to_numeric), unit='s'))
+    historical_fcsts = pd.DataFrame(historical_fcsts)
+    historical_fcsts = historical_fcsts.set_index(pd.to_datetime(historical_fcsts['timestamp'].apply(pd.to_numeric), unit='s'))
+    print(f'retrieved {len(historical_fcsts.index)} historical forecast observations')
 
   df = pd.concat([hist, fcst, historical_fcsts]).sort_index()
   df = df[df['timestamp'] > start_ts]
